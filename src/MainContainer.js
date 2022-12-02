2f5535a790d5d3503dc7ab2634ffe3a0aec9c2b3
Tinatin <tinatinatsikobadze@gmail.com>
2022-12-02 12:46:26 +0400

    initialization

---
+++ src/MainContainer.js
@@ -0,0 +1,12 @@
+import "./MainContainer.css";
+import Card from "./Card";
+import IconDiv from "./icon/IconDiv";
+
+export default function MainContainer() {
+  return (
+    <div className="MainContainer">
+      <Card />
+      <IconDiv />
+    </div>
+  );
+}
---
+++ src/icon/IconDiv.js
@@ -0,0 +1,9 @@
+import star from "./icon/Path.png";
+
+export default function IconDiv() {
+  return (
+    <div>
+      <img src={star} alt="" />
+    </div>
+  );
+}
